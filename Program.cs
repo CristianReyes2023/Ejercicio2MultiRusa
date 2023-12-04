1355bedc8e1de25c0cda10cba51d9ae893df78f2
CristianReyes2023 <137292974+CristianReyes2023@users.noreply.github.com>
2023-12-04 15:42:36 -0500

    feat: :rocket: Point 2 Finished

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa el multiplicador");
+        var multiplicador = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese el multiplicando");
+        var multiplicando = Convert.ToUInt32(Console.ReadLine());
+        ArrayList suma = new ArrayList();
+        bool AddBreak = true;
+        while (AddBreak)
+        {
+            var multiInterador = multiplicador % 2;
+            if (multiInterador == 1)
+            {
+                suma.Add(multiplicando);
+                multiplicador = (multiplicador - 1) / 2;
+            }
+            if(multiInterador == 0)
+            {
+            multiplicador = (multiplicador) / 2;
+            }
+            multiplicando = multiplicando*2;
+            if (multiplicador == 1)
+            {
+                AddBreak = false;
+                suma.Add(multiplicando);
+                var sumaTotal = 0;
+                for(int i=0;i<suma.Count;i++)
+                {
+                    sumaTotal = sumaTotal + Convert.ToInt32(suma[i]);
+                }
+                Console.WriteLine($"El valor de la suma total es: {sumaTotal}");
+            }
+
+        }
+    }
+}
